7355d0f007b250d95a1288894c1c6c9bfdcb92c7
jceballosc <74331355+jceballosc@users.noreply.github.com>
2022-08-20 17:35:43 -0400

    Enemy shoots to main player
    Enemy shoots at random intervals to player.

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+    [Header("General")]
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] float projectileSpeed = 10f;
     [SerializeField] float projectileLifeTime = 5f;
-    [SerializeField] float firingRate = 0.2f;
+    [SerializeField] float baseFiringRate = 0.2f;
+    [Header("AI")]
+    [SerializeField] bool useAI;
+    [SerializeField] float firingRateVariance = 0f;
+    [SerializeField] float minimumFiringRate = .1f;
 
-    public bool isFiring;
+    [HideInInspector] public bool isFiring;
     Coroutine firingCoroutine;
+
     void Start()
     {
-        
+        if (useAI)
+        {
+            isFiring = true;
+        }
     }
 
     
@@ -48,7 +57,11 @@ public class Shooter : MonoBehaviour
                 rb.velocity = transform.up * projectileSpeed;
             }
             Destroy(instance, projectileLifeTime);
-            yield return new WaitForSeconds(firingRate); 
+
+            float timeToNextProjectile = Random.Range(baseFiringRate - firingRateVariance,
+                                                        baseFiringRate + firingRateVariance);
+            timeToNextProjectile = Mathf.Clamp(timeToNextProjectile, minimumFiringRate, float.MaxValue);
+            yield return new WaitForSeconds(timeToNextProjectile); 
         }
     }
 }
