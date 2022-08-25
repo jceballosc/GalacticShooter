b42f124f2a50270e0291ec13e43047ded906f650
jceballosc <74331355+jceballosc@users.noreply.github.com>
2022-08-24 22:05:39 -0400

    Added Sound effects + Particle effects

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -16,6 +16,12 @@ public class Shooter : MonoBehaviour
 
     [HideInInspector] public bool isFiring;
     Coroutine firingCoroutine;
+    AudioPlayer audioPlayer;
+
+    void Awake()
+    {
+        audioPlayer = FindObjectOfType<AudioPlayer>();
+    }
 
     void Start()
     {
@@ -61,6 +67,8 @@ public class Shooter : MonoBehaviour
             float timeToNextProjectile = Random.Range(baseFiringRate - firingRateVariance,
                                                         baseFiringRate + firingRateVariance);
             timeToNextProjectile = Mathf.Clamp(timeToNextProjectile, minimumFiringRate, float.MaxValue);
+            audioPlayer.PlayShootingClip();
+
             yield return new WaitForSeconds(timeToNextProjectile); 
         }
     }
